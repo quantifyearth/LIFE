08706de0cbd56927cde9eda8e4216dbf79aa128a
Michael Dales <mwd24@cam.ac.uk>
2022-11-02 11:31:03 +0000

    Lots of fixes in the face of real data.

--- test.py
+++ test.py
@@ -2,27 +2,27 @@ import itertools
 import json
 import math
 import os
-from multiprocessing import Pool, cpu_count
+import re
+import tempfile
 import time
+from multiprocessing import Pool, cpu_count
 
 import h3
 import numpy as np
-from osgeo import ogr
+from osgeo import ogr, gdal
 from yirgacheffe.layers import Layer, DynamicVectorRangeLayer, PixelScale, UniformAreaLayer
+from yirgacheffe.window import Area, Window
 from yirgacheffe.h3layer import H3CellLayer
 
-
-# CURRENT_RASTERS_DIR = "/maps/results/alison/mammal_examples/current"
 CURRENT_RASTERS_DIR = "/maps/results/alison/mammal_current_raster/"
 AOH_VALUES_CSV = "/home/ae491/dev/persistence-calculator/mammal_example_P.csv"
 RANGE_FILE = "/maps/biodiversity/mammals_extinct_final.gpkg"
+# RANGE_FILE = "/maps-priv/maps/mwd24/mammals_terrestrial_filtered_collected_fix.gpkg"
+OUTPUT_DIR = "/maps/mwd24/h3results"
 
-#  [17975, 15951, 19353, 29673, 41686]
-ID_LIST = [15251, ]#17975, 15951, 19353, 29673, 41686]
+MAG = 7
 
-MAG = 8
-
-LONG_BAND_WIDTH = 2.0
+LONG_BAND_WIDTH = 1.0
 
 def geometry_to_pointlist(geo):
     points = []
@@ -49,11 +49,10 @@ def geometry_to_polygons(geo, subdivide=True, prefix=''):
             if (longitude_width < LONG_BAND_WIDTH) or not subdivide:
                 polygons.append(polygon)
                 continue
-                
+
             # This poly is quite wide, so split it into smaller chunks
             # OGR is quite slow (relative to the test of the work here)
             # so we just do a simple lat banding
-            
             for i in range(math.ceil(longitude_width / LONG_BAND_WIDTH)):
                 left = envelope[0] + (i * LONG_BAND_WIDTH)
                 right = envelope[0] + ((i + 1) * LONG_BAND_WIDTH)
@@ -61,11 +60,11 @@ def geometry_to_polygons(geo, subdivide=True, prefix=''):
                     'type': 'POLYGON',
                     'coordinates': [
                         [
-                            [left, 90.0],
-                            [right, 90.0],
-                            [right, -90.0],
-                            [left, -90.0],
-                            [left, 90.0],
+                            [left, envelope[3]],
+                            [right, envelope[3]],
+                            [right, envelope[2]],
+                            [left, envelope[2]],
+                            [left, envelope[3]],
                         ]
                     ]
                 }
@@ -85,8 +84,19 @@ def geometry_to_polygons(geo, subdivide=True, prefix=''):
         polygon = h3.Polygon(*points)
         return [polygon]
 
-    else:
+    elif geotype == ogr.wkbGeometryCollection:
+        count = geo.GetGeometryCount()
+        polygons = []
+        for i in range(count):
+            polygons += geometry_to_polygons(geo.GetGeometryRef(i), subdivide=False)
+        return polygons
+
+    elif geotype == ogr.wkbPoint:
+        print(geo)
         return []
+
+    else:
+        raise ValueError(f"unknown type {geotype}: {get.GetGeometryName()}")
 
 def polygon_to_tiles(polygon):
 
@@ -96,19 +106,55 @@ def polygon_to_tiles(polygon):
     tiles = h3.polygon_to_cells(polygon, MAG)
     list_of_tiles.append(tiles)
 
-    # now for every vertice on the polygon, work use the minimum distance path to approximate 
+    # now for every vertice on the polygon, work use the minimum distance path to approximate
     # all cells on the boundry
     polygons = [polygon.outer] + list(polygon.holes)
     for loop in polygons:
         if loop[0] != loop[-1]:
             loop.append(loop[0])
         for i in range(len(loop) - 1):
-            start = h3.latlng_to_cell(*(loop[i]), MAG)
-            end = h3.latlng_to_cell(*(loop[i + 1]), MAG)
-            line = h3.grid_path_cells(start, end)
+            start = loop[i]
+            end = loop[i + 1]
+            start_cell = h3.latlng_to_cell(*start, MAG)
+            end_cell = h3.latlng_to_cell(*end, MAG)
+
+            line = [start_cell, end_cell]
+            
+            if start_cell != end_cell:
+                try:
+                    distance_estimate = h3.grid_distance(start_cell, end_cell)
+                except Exception as e:
+                    # if the distance is too far then h3 will give up
+                    # this is usually along the boundaries added by 
+                    # the chunking we do to let us parallelise things, and so
+                    # we don't mi9d, as the polygon_to_cell is sufficient there
+                    print(f'Failed to find path from {start} to {end}: {e}')
+                    continue
+
+                # In an ideal world we'd use h3.grid_path_cells() for this, but in some places
+                # we observe that this does not take the direct route, and the docs do not
+                # say that it'll produce an optimal output, nor that the results are stable.
+                # Instead we do this appoximation by hand, which isn't guaranteed to generate
+                # a contiguous line of cells, but is good enough, particularly once we add
+                # cell padding, which we did anyway even on the original implementation that
+                # had h3.grid_path_cells()
+                if distance_estimate:
+                    diffs = ((end[0] - start[0]) / float(distance_estimate), (end[1] - start[1]) / float(distance_estimate))
+                    for i in range(distance_estimate):
+                        here = (start[0] + (diffs[0] * i), start[1] + (diffs[1] * i))
+                        cell = h3.latlng_to_cell(*here, MAG)
+                        assert h3.is_valid_cell(cell)
+                        line.append(cell)
+                else:
+                    line = h3.grid_path_cells(
+                        h3.latlng_to_cell(*start, MAG),
+                        h3.latlng_to_cell(*end, MAG)
+                    )
+
             list_of_tiles.append(line)
             for cell in line:
-                list_of_tiles.append(h3.grid_ring(cell, 1))
+                list_of_tiles.append(h3.grid_disk(cell, 3))
+
 
     tiles = itertools.chain.from_iterable(list_of_tiles)
 
@@ -131,12 +177,20 @@ def process_cell(args):
     except ValueError:
         return (tile, 0.0)
     for layer in layers:
-        layer.set_window_for_intersection(intersection)
+        try:
+            layer.set_window_for_intersection(intersection)
+        except:
+            print(f'Failed to intersect {tile} with for {layer} with area {layer.area} and {intersection}')
+            raise
 
     # work out area of habitate contributed by just that cell
     calc = aoh_layer * tile_layer
-    tile_aoh = calc.sum()
-    
+    try:
+        tile_aoh = calc.sum()
+    except:
+        print(f' Failed to process {tile} with {intersection} at scale {aoh_layer.pixel_scale}')
+        raise
+
     return (tile, tile_aoh)
 
 def process_pure_cell(args):
@@ -160,16 +214,16 @@ def process_pure_cell(args):
     # work out area of habitate contributed by just that cell
     calc = area_layer * tile_layer
     tile_aoh = calc.sum()
-    
+
     return (tile, tile_aoh)
-    
+
 
 def tiles_to_area(aoh_layer_path, species_id, tiles, s2):
     # we now have all the tiles, so work out the AoH in just that tile
     results = []
     args = [(aoh_layer_path, tile) for tile in tiles]
 
-    with Pool(cpu_count()) as p:
+    with Pool(processes=cpu_count()) as p:
         results = p.map(process_cell, args)
 
     s3 = time.time()
@@ -177,7 +231,7 @@ def tiles_to_area(aoh_layer_path, species_id, tiles, s2):
 
     total = 0.0
     max = 0.0
-    with open(f'res_{species_id}_{MAG}.csv', 'w') as f:
+    with open(os.path.join(OUTPUT_DIR, f'res_{species_id}_{MAG}.csv'), 'w') as f:
         for result in results:
             total += result[1]
             if result[1] > max:
@@ -185,46 +239,74 @@ def tiles_to_area(aoh_layer_path, species_id, tiles, s2):
             f.write(f'{result[0]}, {result[1]},\n')
 
     end = time.time()
-    print(f'total: {total}')
     print(f'max: {max}')
     print(f'Wrote out results in {end - s3} seconds')
+    return total
 
 
+def get_original_aoh_info(raster_path: str):
+    aoh_layer = Layer.layer_from_file(aoh_layer_path)
+    return aoh_layer.sum()
+
+
+def get_range_polygons(range_path, species_id):
+    where_filter = f"id_no = {species_id} and season = 'resident'"
+
+    # The pixel scale and projection don't matter here, as we're just 
+    # abusing yirgacheffe to load the range file. Feels like I need to split this
+    # out at some point
+    layer = DynamicVectorRangeLayer(range_path, where_filter, PixelScale(0.1, 0.1), "UNUSED")
+    range_layer = layer.range_layer
+    range_layer.ResetReading()
+    polygons = []
+    feature = range_layer.GetNextFeature()
+    while feature:
+        geo = feature.GetGeometryRef()
+        polygons.append(geometry_to_polygons(geo, subdivide=True))
+        feature = range_layer.GetNextFeature()
+    return list(itertools.chain.from_iterable(polygons))
+    
 
 if __name__ == "__main__":
 
-    for species_id in ID_LIST:
+    filere = re.compile('^Seasonality.RESIDENT-(\d+).tif$')
+
+    species_list = [filere.match(x).groups()[0] for x in os.listdir(CURRENT_RASTERS_DIR) if filere.match(x)]
+    species_list.sort()
+
+    # for test run, just do first dozen
+    for species_id in species_list[:256]:
         print(species_id)
         start = time.time()
         aoh_layer_path = os.path.join(CURRENT_RASTERS_DIR, f'Seasonality.RESIDENT-{species_id}.tif')
-        aoh_layer = Layer.layer_from_file(aoh_layer_path)
 
-        where_filter = f"id_no = {species_id} and season = 'resident'"
-        layer = DynamicVectorRangeLayer(RANGE_FILE, where_filter, aoh_layer.pixel_scale, aoh_layer.projection)
-        range_layer = layer.range_layer
-        range_layer.ResetReading()
+        # We can't currently parallelise either of these tasks, but they are independant, so we can 
+        # at least run them concurrently...
+        with Pool(processes=cpu_count()) as p:
+            res_aoh_total = p.apply_async(get_original_aoh_info, (aoh_layer_path,))
+            res_polygons = p.apply_async(get_range_polygons, (RANGE_FILE, species_id))
 
-        # Get the polygons from OGR - this can not be done concurrently, as osgeo and multiprocessing
-        # are not friends
-        polygons = []
-        feature = range_layer.GetNextFeature()
-        while feature:
-            geo = feature.GetGeometryRef()
-            polygons += geometry_to_polygons(geo, subdivide=True)
-            feature = range_layer.GetNextFeature()
+            aoh_layer_total = res_aoh_total.get()
+            polygons = res_polygons.get()
+
         s1 = time.time()
         print(f"Found {len(polygons)} polygons in {s1 - start} seconds")
-        
+
         # The h3 lookup can be ran concurrently thought
         tiles = set()
-        with Pool(cpu_count()) as p:
+        with Pool(processes=cpu_count()) as p:
             results = p.map(polygon_to_tiles, polygons)
             tiles = set(itertools.chain.from_iterable(results))
 
         s2 = time.time()
         print(f"Found {len(tiles)} tiles in {s2 - s1} seconds")
 
-        tiles_to_area(aoh_layer_path, species_id, tiles, s2)
+        total = tiles_to_area(aoh_layer_path, species_id, tiles, s2)
+        print(f'AoH layer total: {aoh_layer_total}')
+        print(f'Hex tile total:  {total}')
+
+        diff = ((total - aoh_layer_total) / aoh_layer_total) * 100.0
+        print(f'Error is {diff:.5f}%')
 
         end = time.time()
         print(f'{species_id} at mag {MAG} took {end - start} seconds')
