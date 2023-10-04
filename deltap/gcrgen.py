bfac84ce6702cfd90bfcaa4ff9377b2b45dec4af
Michael Dales <mwd24@cam.ac.uk>
2023-10-04 09:19:24 +0100

    Switch from Tom's to Ali's gcrgen.py

--- deltap/gcrgen.py
+++ deltap/gcrgen.py
@@ -9,16 +9,13 @@ messy kwargs, but can be adapted fairly easily.
 import os
 import argparse
 import pandas as pd
+import warnings
 
-classes = [
-            "bow", 
-           "mammals", 
-           "amphibians", 
-           "reptiles"]
-# season = "RESIDENT"
-habmaps = {"historic"   : "hist",
-           "scenario"   : "noag_f_pnv",
-           "current"    : "noag_f_gh"
+classes = ["birds", "mammals", "amphibians", "reptiles"]
+season = "RESIDENT"
+habmaps = {"historic"   : "pnv",
+           "scenario"   : "restore",
+           "current"    : "current_L1L2"
            }
 habmaps_r = {v: k for k, v in habmaps.items()}
 
@@ -44,10 +41,8 @@ parser.add_argument('--target_dir',
 parser.add_argument('--output_dir',
                     type = str, help = "where to save the csv",
                     required = True, dest = "output_dir")
-parser.add_argument('--findext',
-                    type = str,
-                    required = True, dest = "findext")
 args = vars(parser.parse_args())
+
 
 for c in classes:
     os.makedirs(os.path.join(args["output_dir"], c), exist_ok = True)
@@ -56,11 +51,7 @@ f = []
 for path, subdirs, files in os.walk(args["target_dir"]):
     for name in files:
         f.append(os.path.join(path, name))
-
-if args["findext"] != None:
-    f = [file for file in f if args["findext"] in file]
-else:
-    f = [file for file in f if ".csv" in file]
+f = [file for file in f if ".csv" in file]
 
 df = pd.DataFrame()
 index_levels = ["taxid", "season", "taxclass"]
@@ -86,28 +77,21 @@ for i, file in enumerate(f):
 
 df = df.reset_index()
 if "historic" not in df.columns:
-    print("No historic .csvs found - adding nan columns")
-    df["historic"] = "nan"
+    df['historic'] = "nan"
+print(df)
+with open(os.path.join(args["output_dir"], "file_index_lj.csv"), "w+") as out_file:
 
-
-try:
-    outfilename = splitall(args['target_dir'])[-2]
-except IndexError:
-    outfilename = args["target_dir"]
-    
-with open(os.path.join(args["output_dir"], f"file_index_{outfilename}.csv"), "w+") as out_file:
     out_file.write("--current_path,--scenario_path,--historic_path,--output_path")
     out_file.write("\n")
-    print(f"Writing littlejohn arguments to {os.path.join(args['output_dir'], f'file_index_{outfilename}.csv')}: ", round(i/len(df), 4), end = "\r" )
+
     for i, (idx, row) in enumerate(df.iterrows()):
+        print(f"Writing littlejohn arguments to {os.path.join(args['output_dir'], 'file_index_lj.csv')}: ", round(i/len(df), 4), end = "\r" )
         curr = row.current
         scen = row.scenario
         hist = row.historic
-        if args["findext"] != None:
-            ofname = f"Seasonality.{row.season}-{row.taxid}{args['findext']}"
-        else:
-            ofname = f"Seasonality.{row.season}-{row.taxid}.csv"
+        ofname = f"Seasonality.{row.season}-{row.taxid}.csv"
         of = os.path.join(args["output_dir"], row.taxclass, ofname)
+
         out_file.write(f"{curr},{scen},{hist},{of}")
         out_file.write("\n")
 
