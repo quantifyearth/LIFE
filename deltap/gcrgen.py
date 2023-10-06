dc53205716d3607b70a31d6ef3616ff5a18dd7eb
Michael Dales <mwd24@cam.ac.uk>
2023-10-06 12:52:50 +0100

    Support generating multiple exponent variets.

--- deltap/gcrgen.py
+++ deltap/gcrgen.py
@@ -6,59 +6,53 @@ messy kwargs, but can be adapted fairly easily.
 
 """
 
-import os
 import argparse
+import os
+
 import pandas as pd
-import warnings
-
-classes = ["birds", "mammals", "amphibians", "reptiles"]
-season = "RESIDENT"
-habmaps = {"historic"   : "pnv",
-           "scenario"   : "restore",
-           "current"    : "current_L1L2"
-           }
-habmaps_r = {v: k for k, v in habmaps.items()}
-
-def splitall(path):
-    allparts = []
-    while 1:
-        parts = os.path.split(path)
-        if parts[0] == path:  # sentinel for absolute paths
-            allparts.insert(0, parts[0])
-            break
-        elif parts[1] == path: # sentinel for relative paths
-            allparts.insert(0, parts[1])
-            break
-        else:
-            path = parts[0]
-            allparts.insert(0, parts[1])
-    return allparts
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument('--target_dir',
         type=str,help="Look for folders called 'search' in this directory",
         required=True,dest="target_dir")
+parser.add_argument('--scenario', required=True, dest="scenario")
 parser.add_argument('--output_dir',
                     type = str, help = "where to save the csv",
                     required = True, dest = "output_dir")
 args = vars(parser.parse_args())
 
 
-for c in classes:
-    os.makedirs(os.path.join(args["output_dir"], c), exist_ok = True)
+classes = ["birds", "mammals", "amphibians", "reptiles"]
+z_values = ["gompertz"]
+season = "RESIDENT"
+habmaps = {"historic"   : "pnv",
+           "scenario"   : args["scenario"],
+           "current"    : "current_L1L2"
+           }
+habmaps_r = {v: k for k, v in habmaps.items()}
 
-f = []
+
+target_dir = args["target_dir"]
+
+# for z in z_values:
+#         for taxa in classes:
+#             os.makedirs(os.path.join(args["output_dir"], args["scenario"], str(z), taxa), exist_ok=True)
+os.makedirs(args["output_dir"], exist_ok=True)
+
+tif_file_paths = []
 for path, subdirs, files in os.walk(args["target_dir"]):
     for name in files:
-        f.append(os.path.join(path, name))
-f = [file for file in f if ".tif" in file]
+        _, ext = os.path.splitext(name)
+        if ext == '.tif':
+            tif_file_paths.append(os.path.join(path, name))
 
 df = pd.DataFrame()
 index_levels = ["taxid", "season", "taxclass"]
 df.index = pd.MultiIndex(levels=[[]] * len(index_levels), codes=[[]] * len(index_levels), names=index_levels)
 
-for i, file in enumerate(f):
-    print("Reading in files: ", round(i/len(f), 4), end = "\r" )
+for i, file in enumerate(tif_file_paths):
+    # print("Reading in files: ", round(i/len(tif_file_paths), 4), end = "\r" )
+
     path, fname = os.path.split(file)
     taxid = fname.split("-")[-1].split(".")[0]
     season = fname.split("-")[0].split(".")[-1]
@@ -74,26 +68,26 @@ for i, file in enumerate(f):
             c2 += 1
     if c1 == 1 and c2 == 1:
         df.loc[(taxid, season, taxclass), habmaps_r[habmap]] = file
-
 df = df.reset_index()
 if "historic" not in df.columns:
     df['historic'] = "nan"
-print(df)
-with open(os.path.join(args["output_dir"], "file_index_lj.csv"), "w+") as out_file:
 
-    out_file.write("--current_path,--scenario_path,--historic_path,--output_path")
+filename = os.path.join(args["output_dir"], f"g_file_index_{args['scenario']}_lj.csv")
+with open(filename, "w+") as out_file:
+    out_file.write("--current_path,--scenario_path,--historic_path,--output_path,--z")
     out_file.write("\n")
 
     for i, (idx, row) in enumerate(df.iterrows()):
-        print(f"Writing littlejohn arguments to {os.path.join(args['output_dir'], 'file_index_lj.csv')}: ", round(i/len(df), 4), end = "\r" )
-        curr = row.current
-        scen = row.scenario
-        hist = row.historic
-        ofname = f"Seasonality.{row.season}-{row.taxid}.tif"
-        of = os.path.join(args["output_dir"], row.taxclass, ofname)
+        for z in z_values:
+            print(f"Writing littlejohn arguments to {filename}: ", round(i/len(df), 4), end = "\r" )
+            curr = row.current
+            scen = row.scenario
+            hist = row.historic
+            ofname = f"Seasonality.{row.season}-{row.taxid}.tif"
+            of = os.path.join(args["output_dir"], args["scenario"], str(z), row.taxclass, ofname)
 
-        out_file.write(f"{curr},{scen},{hist},{of}")
-        out_file.write("\n")
+            out_file.write(f"{curr},{scen},{hist},{of},{str(z)}")
+            out_file.write("\n")
 
 
     
--- deltap/global_code_residents_pixel_AE_64.py
+++ deltap/global_code_residents_pixel_AE_64.py
@@ -14,6 +14,7 @@ This isn't tidied or commented properly
 """
 import argparse
 import os
+import math
 import re
 import sys
 import warnings
@@ -29,12 +30,22 @@ from yirgacheffe.layers import RasterLayer, ConstantLayer
 quiet = False
 overwrite = True
 
+gompertz_a = 2.5
+gompertz_b = -14.5
+gompertz_alpha = 1
+
+def gen_gompertz(x,):
+  return math.exp(-math.exp(gompertz_a + (gompertz_b * (x ** gompertz_alpha))))
+
+def numpy_gompertz(x):
+    return np.exp(-np.exp(gompertz_a + (gompertz_b * (x ** gompertz_alpha))))
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--current_path',
     type=str,required=True,dest="current_path",
     help="path to species current AOH hex")
 parser.add_argument('--historic_path',
-    type=str,required=True,dest="historic_path",
+    type=str,required=False,dest="historic_path",
     help="path to species historic AOH hex")
 parser.add_argument('--scenario_path',
     type=str,required=True,dest="scenario_path",
@@ -42,13 +53,30 @@ parser.add_argument('--scenario_path',
 parser.add_argument('--output_path',
     type=str,required=True,dest="output_path",
     help="path to save output csv")
+parser.add_argument('--z', dest='exponent', type=str, default='0.25')
 parser.add_argument('-ht', '--hist_table',
                     dest = "hist_table",
                     type = str)
 args = vars(parser.parse_args())
 
+try:
+    exp_val = float(args['exponent'])
+    z_exponent_func_float = lambda x: x ** exp_val
+    z_exponent_func_raster = lambda x: x ** exp_val
+except ValueError:
+    if args['exponent'] == "gompertz":
+        z_exponent_func_float = gen_gompertz
+        z_exponent_func_raster = numpy_gompertz
+    else:
+        quit(f"unrecognised exponent {args['exponent']}")
+
+if (not 'historic_path' in args.keys()) and (not 'hist_table' in args.keys()):
+    quit("Please provide either historic_path or hist_table arguments")
+
 if not overwrite and os.path.isfile(args['output_path']):
     quit(f"{args['output_path']} exists, set overwrite to False to ignore this.")
+path, _ = os.path.split(args["output_path"])
+os.makedirs(path, exist_ok=True)
 
 FILERE = re.compile(r'.*Seasonality.(\w+)-(\d+).tif$')
 season, taxid = FILERE.match(args['current_path']).groups()
@@ -64,8 +92,8 @@ def open_layer_as_float64(filename: str) -> RasterLayer:
     return layer64
 
 
-def calc_persistence_value(current_AOH: float, historic_AOH: float, exponent: float) -> float:
-    sp_P = (current_AOH / historic_AOH) ** exponent
+def calc_persistence_value(current_AOH: float, historic_AOH: float, exponent_func) -> float:
+    sp_P = exponent_func(current_AOH / historic_AOH)
     sp_P_fix = np.where(sp_P > 1, 1, sp_P)
     return sp_P_fix
 
@@ -79,7 +107,7 @@ def process_delta_p(current: RasterLayer, scenario: RasterLayer, current_AOH: fl
     new_AOH = RasterLayer.empty_raster_layer_like(current)
     calc_1.save(new_AOH)
 
-    calc_2 = ((new_AOH / historic_AOH) ** 0.25) #SECOND CALCULATION: NEW PERSISTENCE
+    calc_2 = (new_AOH / historic_AOH).numpy_apply(z_exponent_func_raster)
     calc_2 = calc_2.numpy_apply(lambda chunk: np.where(chunk > 1, 1, chunk))
     new_p = RasterLayer.empty_raster_layer_like(new_AOH)
     calc_2.save(new_p)
@@ -108,7 +136,7 @@ if season == 'resident':
 
     new_p_layer = process_delta_p(current, scenario, current_AOH, historic_AOH)
 
-    old_persistence = calc_persistence_value(current_AOH, historic_AOH, 0.25)
+    old_persistence = calc_persistence_value(current_AOH, historic_AOH, z_exponent_func_float)
     calc = new_p_layer - ConstantLayer(old_persistence)
     delta_p = RasterLayer.empty_raster_layer_like(new_p_layer, filename=args['output_path'])
     calc.save(delta_p)
@@ -142,15 +170,16 @@ elif season == 'nonbreeding':
     historic_AOH_breeding = hdf[(hdf.id_no == int(taxid))&(hdf.season == " " + 'breeding')].AOH.values[0]
     if historic_AOH_breeding == 0.0:
         quit(f"Historic AoH breeding for {taxid} is zero, aborting")
-    persistence_breeding = calc_persistence_value(current_AOH_breeding, historic_AOH_breeding, 0.25)
+    persistence_breeding = calc_persistence_value(current_AOH_breeding, historic_AOH_breeding, z_exponent_func_float)
 
     current_AOH_non_breeding = current_non_breeding.sum()
     historic_AOH_non_breeding = hdf[(hdf.id_no == int(taxid))&(hdf.season == " " + 'nonbreeding')].AOH.values[0]
     if historic_AOH_non_breeding == 0.0:
         quit(f"Historic AoH for non breeding {taxid} is zero, aborting")
-    persistence_non_breeding = calc_persistence_value(current_AOH_non_breeding, historic_AOH_non_breeding, 0.25)
+    persistence_non_breeding = calc_persistence_value(current_AOH_non_breeding, historic_AOH_non_breeding, z_exponent_func_float)
 
     old_persistence = (persistence_breeding ** 0.5) * (persistence_non_breeding ** 0.5)
+    print(old_persistence)
 
     new_p_breeding = process_delta_p(current_breeding, scenario_breeding, current_AOH_breeding, historic_AOH_breeding)
     new_p_non_breeding = process_delta_p(current_non_breeding, scenario_non_breeding, current_AOH_non_breeding, historic_AOH_non_breeding)
@@ -162,3 +191,4 @@ elif season == 'nonbreeding':
     output = RasterLayer.empty_raster_layer_like(new_p_breeding, filename=args['output_path'])
     delta_p_layer.save(output)
 
+    print(delta_p_layer.sum())
