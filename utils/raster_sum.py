d3e06019d5433abc59eba2a1c66a7dc46917f5bd
Michael Dales <mwd24@cam.ac.uk>
2025-07-24 13:51:49 +0100

    Improve performance of raster_sum.py a little

--- utils/raster_sum.py
+++ utils/raster_sum.py
@@ -1,61 +1,58 @@
 import argparse
-import glob
 import os
+import queue
 import sys
 import tempfile
 import time
+from pathlib import Path
 from multiprocessing import Manager, Process, Queue, cpu_count
+from typing import Optional
 
-import numpy as np
-from yirgacheffe.layers import RasterLayer, YirgacheffeLayer  # type: ignore
-from osgeo import gdal
+from yirgacheffe.layers import RasterLayer  # type: ignore
+from yirgacheffe.operators import DataType
 
 def worker(
+    compress: bool,
     filename: str,
-    result_dir: str,
+    result_dir: Path,
     input_queue: Queue,
 ) -> None:
-    output_tif = os.path.join(result_dir, filename)
+    output_tif = result_dir / filename
 
-    merged_result = None
+    merged_result: Optional[RasterLayer] = None
 
     while True:
-        path = input_queue.get()
-        if path is None:
+        try:
+            path: Path = input_queue.get_nowait()
+        except queue.Empty:
             break
+        if compress:
+            print(path)
 
         with RasterLayer.layer_from_file(path) as partial_raster:
             if merged_result is None:
-                merged_result = RasterLayer.empty_raster_layer_like(partial_raster, datatype=gdal.GDT_Float64)
-                cleaned_raster = partial_raster.numpy_apply(lambda chunk: np.nan_to_num(chunk, copy=False, nan=0.0))
+                merged_result = RasterLayer.empty_raster_layer_like(partial_raster, datatype=DataType.Float64)
+                cleaned_raster = partial_raster.nan_to_num(nan=0.0)
                 cleaned_raster.save(merged_result)
             else:
-                merged_result.reset_window()
-
-                union = YirgacheffeLayer.find_union([merged_result, partial_raster])
-                merged_result.set_window_for_union(union)
-                partial_raster.set_window_for_union(union)
-
-                calc = merged_result + (
-                    partial_raster.numpy_apply(lambda chunk: np.nan_to_num(chunk, copy=False, nan=0.0))
-                )
-                temp = RasterLayer.empty_raster_layer_like(merged_result, datatype=gdal.GDT_Float64)
+                calc = merged_result + partial_raster.nan_to_num(nan=0.0)
+                temp = RasterLayer.empty_raster_layer_like(calc, datatype=DataType.Float64)
                 calc.save(temp)
                 merged_result = temp
 
     if merged_result:
-        final = RasterLayer.empty_raster_layer_like(merged_result, filename=output_tif)
-        merged_result.save(final)
+        with RasterLayer.empty_raster_layer_like(merged_result, filename=output_tif, compress=compress) as final:
+            merged_result.save(final)
+        input_queue.put(output_tif)
 
-def build_k(
-    images_dir: str,
-    output_filename: str,
+def raster_sum(
+    images_dir: Path,
+    output_filename: Path,
     processes_count: int
 ) -> None:
-    result_dir, filename = os.path.split(output_filename)
-    os.makedirs(result_dir, exist_ok=True)
+    os.makedirs(output_filename.parent, exist_ok=True)
 
-    files = [os.path.join(images_dir, x) for x in glob.glob("*.tif", root_dir=images_dir)]
+    files = images_dir.glob("*.tif")
     if not files:
         sys.exit(f"No files in {images_dir}, aborting")
 
@@ -64,8 +61,9 @@ def build_k(
             source_queue = manager.Queue()
 
             workers = [Process(target=worker, args=(
+                False,
                 f"{index}.tif",
-                tempdir,
+                Path(tempdir),
                 source_queue
             )) for index in range(processes_count)]
             for worker_process in workers:
@@ -73,8 +71,8 @@ def build_k(
 
             for file in files:
                 source_queue.put(file)
-            for _ in range(len(workers)):
-                source_queue.put(None)
+            # for _ in range(len(workers)):
+            #     source_queue.put(None)
 
             processes = workers
             while processes:
@@ -86,19 +84,17 @@ def build_k(
                             victim.kill()
                         sys.exit(candidate.exitcode)
                     processes.remove(candidate)
-                time.sleep(1)
+                time.sleep(0.1)
+
 
             # here we should have now a set of images in tempdir to merge
             single_worker = Process(target=worker, args=(
-                filename,
-                result_dir,
+                True,
+                output_filename.name,
+                output_filename.parent,
                 source_queue
             ))
             single_worker.start()
-            nextfiles = [os.path.join(tempdir, x) for x in glob.glob("*.tif", root_dir=tempdir)]
-            for file in nextfiles:
-                source_queue.put(file)
-            source_queue.put(None)
 
             processes = [single_worker]
             while processes:
@@ -114,20 +110,20 @@ def build_k(
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Generates a single raster of M from all the per K rasters.")
+    parser = argparse.ArgumentParser(description="Sums many rasters into a single raster")
     parser.add_argument(
         "--rasters_directory",
-        type=str,
+        type=Path,
         required=True,
         dest="rasters_directory",
-        help="GeoTIFF file containing pixels in set M as generated by calculate_k.py"
+        help="Folder containing all the deltap rasters for a given scenario in their respective subdirectories."
     )
     parser.add_argument(
         "--output",
-        type=str,
+        type=Path,
         required=True,
         dest="output_filename",
-        help="Destination parquet file for results."
+        help="Destination geotiff file for results."
     )
     parser.add_argument(
         "-j",
@@ -139,7 +135,7 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    build_k(
+    raster_sum(
         args.rasters_directory,
         args.output_filename,
         args.processes_count
